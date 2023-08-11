07338ef5e3a25439c9d626dd72dae4342454a3a0
jrmarcco <jrmarcco@gmail.com>
2023-08-11 12:59:40 +0800

    style:

--- db.go
+++ db.go
@@ -1,80 +1,80 @@
 package orm
 
 import (
-    "context"
-    "database/sql"
-    "github.com/jrmarcco/easy-orm/internal/val"
-    "github.com/jrmarcco/easy-orm/model"
+	"context"
+	"database/sql"
+	"github.com/jrmarcco/easy-orm/internal/val"
+	"github.com/jrmarcco/easy-orm/model"
 )
 
 type DB struct {
-    *Core
-    sqlDB *sql.DB
+	*Core
+	sqlDB *sql.DB
 }
 
 type DBOpt func(db *DB)
 
 func DDWithValCreator(creator val.Creator) DBOpt {
-    return func(db *DB) {
-        db.creator = creator
-    }
+	return func(db *DB) {
+		db.creator = creator
+	}
 }
 
 func DBWithDialect(dialect Dialect) DBOpt {
-    return func(db *DB) {
-        db.dialect = dialect
-    }
+	return func(db *DB) {
+		db.dialect = dialect
+	}
 }
 
 func Open(driver string, dsn string, opts ...DBOpt) (*DB, error) {
-    sqlDB, err := sql.Open(driver, dsn)
-    if err != nil {
-        return nil, err
-    }
+	sqlDB, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
 
-    return OpenDB(sqlDB, opts...)
+	return OpenDB(sqlDB, opts...)
 }
 
 func OpenDB(sqlDB *sql.DB, opts ...DBOpt) (*DB, error) {
 
-    core := &Core{
-        registry: model.NewRegistry(),
-        creator:  val.NewUnsafeValWriter,
-        dialect:  StandardSQL,
-    }
+	core := &Core{
+		registry: model.NewRegistry(),
+		creator:  val.NewUnsafeValWriter,
+		dialect:  StandardSQL,
+	}
 
-    db := &DB{
-        sqlDB: sqlDB,
-        Core:  core,
-    }
+	db := &DB{
+		sqlDB: sqlDB,
+		Core:  core,
+	}
 
-    for _, opt := range opts {
-        opt(db)
-    }
+	for _, opt := range opts {
+		opt(db)
+	}
 
-    return db, nil
+	return db, nil
 }
 
 func (d *DB) getCore() *Core {
-    return d.Core
+	return d.Core
 }
 
 func (d *DB) queryContext(ctx context.Context, sql string, args ...any) (*sql.Rows, error) {
-    return d.sqlDB.QueryContext(ctx, sql, args...)
+	return d.sqlDB.QueryContext(ctx, sql, args...)
 }
 
 func (d *DB) execContext(ctx context.Context, sql string, args ...any) (sql.Result, error) {
-    return d.sqlDB.ExecContext(ctx, sql, args...)
+	return d.sqlDB.ExecContext(ctx, sql, args...)
 }
 
 func (d *DB) beginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
-    sqlTx, err := d.sqlDB.BeginTx(ctx, opts)
-    if err != nil {
-        return nil, err
-    }
+	sqlTx, err := d.sqlDB.BeginTx(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
 
-    return &Tx{
-        Core:  d.Core,
-        sqlTx: sqlTx,
-    }, nil
+	return &Tx{
+		Core:  d.Core,
+		sqlTx: sqlTx,
+	}, nil
 }
